a2f9f35c28c8925654e972c1682b8f065ca4c7a5
itsnine <fid4nn@gmail.com>
2021-09-25 12:31:25 +0500

    added try catch

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
     const float iouThreshold = 0.4f;
 
     std::string modelPath = "yolov5m.onnx";
-    std::string imagePath = "zidane.jpg";
+    std::string imagePath = "bus.jpg";
     std::string classNamesPath = "coco.names";
 
     if (argc == 4)
@@ -24,10 +24,23 @@ int main(int argc, char* argv[])
 
     std::vector<std::string> classNames = utils::loadNames(classNamesPath);
 
+    if (classNames.empty())
+    {
+        std::cerr << "Error: Empty class names file." << std::endl;
+        return -1;
+    }
+
     YOLODetector detector {nullptr};
+    cv::Mat image;
+    std::vector<Detection> result;
+
     try
     {
         detector = YOLODetector(modelPath, true, cv::Size(640, 640));
+        std::cout << "Model was initialized." << std::endl;
+    
+        image = cv::imread(imagePath);
+        result = detector.detect(image, confThreshold, iouThreshold);
     }
     catch(const std::exception& e)
     {
@@ -35,13 +48,9 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    cv::Mat image = cv::imread(imagePath);
-    std::vector<Detection> result = detector.detect(image, confThreshold, iouThreshold);
-
     utils::visualizeDetection(image, result, classNames);
 
     cv::imshow("result", image);
-
     // cv::imwrite("result.jpg", image);
     cv::waitKey(0);
 
